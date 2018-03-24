c8b77738ff7c7853cf09acdeaa72653409e771c9
vadimzgonnik <vadimzgonnik95@gmail.com>
2018-03-24 20:51:24 +0200

    add slider

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
-// function range() {
-//     var val = $('.range').val();
-//     $('.range').css({ ' background': '-webkit-linear-gradient(left, #1db1ae 0%, #1db1ae +val+, #e4e4e4 +val+, #e4e4e4 100%)' })
-// }
-
-var range = document.getElementById('range');
-var val = range.value;
-
-range.oninput = function() {
-    range.style.background = '-webkit-linear-gradient(left, #1db1ae 0%, #1db1ae +val, #e4e4e4 +val, #e4e4e4 100%)'
-}
+$(document).ready(() => {
+    $('input[type="range"]').on('input', (e) => {
+        const value = e.target.value;
+        $(e.target).css('background', `-webkit-linear-gradient(left, #1db1ae 0%, #1db1ae ${value}%, #e4e4e4 ${value}%, #e4e4e4 100%)`)
+    })
+})
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel();
+});
