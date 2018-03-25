507133620fdc6c02d2ddbc85d0530733924b60d0
vadimzgonnik <vadimzgonnik95@gmail.com>
2018-03-25 15:38:08 +0300

    working to carousel and footer

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ $(document).ready(() => {
         const value = e.target.value;
         $(e.target).css('background', `-webkit-linear-gradient(left, #1db1ae 0%, #1db1ae ${value}%, #e4e4e4 ${value}%, #e4e4e4 100%)`)
     })
+    $('.sl').slick();
 })
-$(document).ready(function() {
-    $(".owl-carousel").owlCarousel();
-});
+$(document).ready(() => {
+
+    $('.sl').slick();
+})
