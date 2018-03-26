2fc6bf38620e3f3c7ec3f601b5989eeb75b1e296
vadimzgonnik <vadimzgonnik95@gmail.com>
2018-03-26 18:51:46 +0300

    end

--- js/main.js
+++ js/main.js
@@ -3,9 +3,47 @@ $(document).ready(() => {
         const value = e.target.value;
         $(e.target).css('background', `-webkit-linear-gradient(left, #1db1ae 0%, #1db1ae ${value}%, #e4e4e4 ${value}%, #e4e4e4 100%)`)
     })
-    $('.sl').slick();
-})
-$(document).ready(() => {
 
-    $('.sl').slick();
 })
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+
+    showSlides(slideIndex += n);
+    // console.log(slideIndex);
+}
+
+function minusSlides(n) {
+    showSlides(slideIndex += n);
+    // console.log(slideIndex);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+
+    }
+    slides[slideIndex - 1].style.display = "block";
+    // dots[slideIndex - 1].className += " active";
+}
